4576a8f851041b83c86942d9c4d54d91026eb623
merinpwilson <merinwilson2004@gmail.com>
2024-06-29 14:32:09 +0530

    submited

--- app/src/main/java/com/example/friendsapp/MainActivity.java
+++ app/src/main/java/com/example/friendsapp/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.friendsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText e1,e2,e3,e4;
+    AppCompatButton b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        e1=(EditText) findViewById(R.id.name);
+        e2=(EditText) findViewById(R.id.fname);
+        e3=(EditText) findViewById(R.id.nickname);
+        e4=(EditText) findViewById(R.id.description);
+        b1=(AppCompatButton) findViewById(R.id.submitbtn);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getName=e1.getText().toString();
+                String getFname=e2.getText().toString();
+                String getNickname=e3.getText().toString();
+                String getdescription=e4.getText().toString();
+
+                Toast.makeText(getApplicationContext(),getName+" "+getFname+" "+getNickname+" "+getdescription,Toast.LENGTH_LONG).show();;
+            }
         });
     }
 }
